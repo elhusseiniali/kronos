80807a25a9358091d58e146f05fe1b828733de4e
Ali El Husseini <elhusseiniali@icloud.com>
2020-03-08 01:04:45 +0100

    Added email validator for User.

--- code/kronos/models.py
+++ code/kronos/models.py
@@ -213,7 +213,7 @@ class User(db.Model, UserMixin):
 
     username = db.Column(db.String(20), unique=True, nullable=False)
 
-    email = db.Column(db.String(120), unique=True, nullable=False)
+    email = db.Column(EmailType)
     password = db.Column(db.String(60), nullable=False)
 
     def __init__(self, username):
